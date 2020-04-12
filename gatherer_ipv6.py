6a8090e86d775825d745590689221563f1abc7df
Peter Athanas <peter@athanas.org>
2020-04-12 19:50:51 -0400

    Project creation.

---
+++ gatherer_ipv6.py
@@ -0,0 +1,61 @@
+#/usr/bin/python
+
+#########################################################################
+#
+#  FILE: gatherer_ipv6.py
+#
+#  DESCRIPTION: The purpose of this recipe is to gather and count incoming
+#      packets during a limited period of time.
+#
+#  CREATED: 3/2020, P. Athanas, Virginia Tech
+#
+#########################################################################
+
+import socket
+import sys
+import struct
+from subprocess import check_output
+
+LISTEN_PORT = 10000
+
+# Determine my IPV6 address:
+myIP = check_output(['hostname', '-I']).split()[1]
+print("My IPV6 address is "+myIP)
+
+server_address = (myIP, 10000, 0, 0)
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM, 0)
+
+# Bind the socket to the port
+sock.bind(server_address)
+# Listen for incoming connections
+sock.listen(1)
+
+print("Ready for action.")
+pktcount = 0
+
+while True:
+    # Wait for a connection
+    connection, client_address = sock.accept()
+    try:
+        print >>sys.stderr, 'connection from', client_address
+
+        # Receive the data in small chunks and retransmit it
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                print >>sys.stderr, 'no more data from', client_address
+                break
+            flowId = struct.unpack('>I',data[4:8])[0]
+            if flowId != 1: continue
+            seq = struct.unpack('>I',data[8:12])[0]
+            pktcount+=1
+            print(myIP+": From "+str(client_address[0])+": SEQ  = "+str(seq)+ " PKTCOUNT: "+str(pktcount))
+
+
+    finally:
+        # Clean up the connection
+        print("All done.  A total of "+str(pktcount)+" packets were successfully received.")
+        connection.close()
+
